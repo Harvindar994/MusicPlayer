ef61f707abc7e2f10ab4f886bf5f5048976c9123
Harvindar Singh <sjagjeet994@gmail.com>
2022-11-23 18:13:43 +0530

    Now Duration is also getting updated

--- main.js
+++ main.js
@@ -167,7 +167,7 @@ var songElement = function(songFile, songName, artist, duration, image, isLiked,
             <div class="about-song">
                 <h3>${this.name}</h3>
                 <p>${this.artist}</p>
-                <p>${this.duration}</p>
+                <p id="song-duration">${this.duration}</p>
             </div>
             <div class="song-buttons">
                 <a id="unlikeButton" class="liked hide"><i class="fa-solid fa-heart"></i></a>
@@ -227,6 +227,8 @@ var listHandler = function (lContainer, onLike=null, onDelete=null, onSelect=nul
     this.allListElements = [];
     this.isLikeFilterActive = false;
 
+    this.fetchingSongDurationList = [];
+
     this.likeFilterApplyToggel = () => {
         if(this.isLikeFilterActive){
             this.listElements = this.allListElements;
@@ -255,6 +257,28 @@ var listHandler = function (lContainer, onLike=null, onDelete=null, onSelect=nul
         var element = new songElement(songFile, songName, artist, duration, image, isLiked, this.onLike, this.onDelete, this.onSelect);
         this.lContainer.append(element.element);
         this.listElements.push(element);
+        this.fetchingSongDurationList(element);
+    }
+
+    this.fetchingSongDurationList = function(song_element){
+        var audioContext = new Audio(song_element.songFile);
+        audioContext.addEventListener("loadedmetadata", ()=>{
+            var songDuration = song_element.element.querySelector("#song-duration");
+            var duration = this.convertHMS(audioContext.duration);
+            song_element.duration = `${duration[0]>0 ? duration[0] + ":" : ""}${duration[1]>9 ? duration[1] : "0"+duration[1]}:${duration[2]>9 ? duration[2] : "0"+duration[2]}`;
+            songDuration.innerText = song_element.duration;
+            console.log(duration);
+        });
+    }
+
+    this.convertHMS = function(seconds){
+        let duration = parseInt(seconds);
+        let hours = parseInt((duration / 60) / 60);
+        duration -= hours * 3600;
+        let minutes = parseInt(duration / 60);
+        duration -= minutes * 60;
+        let sec = parseInt(duration);
+        return [hours, minutes, sec];
     }
 
     this.onDelete = (song_element) => {
@@ -736,35 +760,3 @@ var audioPlayer = function(browseFileButton, timeSlider, playButton, pauseButton
 
 var audio_player = new audioPlayer(buttonBrowseFile, timeSlider, buttonPlay, buttonPause, buttonNext, buttonPrevious, buttonShuffle, buttonRepeat, songCurrentTime, songDuration);
 audio_player.listHandler.selectByIndex(1);
-
-
-// const jsmediatags = window.jsmediatags;
-
-// document.querySelector("#input").addEventListener("change", (event) => {
-  
-//   const file = event.target.files[0];
-
-//   jsmediatags.read(file, {
-//     onSuccess: function(tag) { 
-
-//       // Array buffer to base64
-//       const data = tag.tags.picture.data;
-//       const format = tag.tags.picture.format;
-//       let base64String = "";
-//       for (let i = 0; i < data.length; i++) {
-//         base64String += String.fromCharCode(data[i]);
-//       }
-
-//       // Output media tags
-//       document.querySelector("#cover").style.backgroundImage = `url(data:${format};base64,${window.btoa(base64String)})`;
-      
-//       document.querySelector("#title").textContent = tag.tags.title;
-//       document.querySelector("#artist").textContent = tag.tags.artist;
-//       document.querySelector("#album").textContent = tag.tags.album;
-//       document.querySelector("#genre").textContent = tag.tags.genre;
-//       },
-//       onError: function(error) {
-//         console.log(error);
-//       }
-//   });  
-// });
